ba260f5d4222739b202d63177d54e10e41f562e7
Aprillia Abel Cleodora <aprilliaabl17@gmail.com>
2026-03-19 13:18:33 +0700

    Menambahkan Praktikum 3

---
+++ Praktikum 3/TugasKeaktifan3_24060124140176_Aprillia Abel Cleodora/Emoticon.cpp
@@ -0,0 +1,204 @@
+/* 
+ * File        : Emoticon.cpp
+ * Deskripsi   : Program OpenGL untuk menampilkan model tangan 3D sederhana
+ *               dengan beberapa pose emoticon dan kontrol rotasi tampilan
+ * Pembuat     : Aprillia Abel Cleodora
+ * Tanggal     : 19 Maret 2026
+ */
+
+#include <windows.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+// Variabel sudut
+static int shoulder1 = 0, shoulder2 = 0, shoulder3 = 0;
+static int elbow = 0, hand = 0;
+static int arm1 = 0, arm2 = 0, arm3 = 0, arm4 = 0, arm5 = 0;
+
+// Rotasi 
+int rotateX = 0, rotateY = 0;
+
+// POSE 
+void poseOpenHand(){
+    arm1 = 0;
+    arm2 = 0;
+    arm3 = 0;
+    arm4 = 0;
+    arm5 = 0;
+}
+
+void poseThumbsUp(){
+    arm1 = 90;
+    arm2 = 90;
+    arm3 = 90;
+    arm4 = 90;
+    arm5 = -60;
+}
+
+void posePeace(){
+    arm1 = 90;
+    arm2 = 90;
+    arm3 = 0;
+    arm4 = 0;
+    arm5 = 20;
+}
+
+void poseFist(){
+    arm1 = 90;
+    arm2 = 90;
+    arm3 = 90;
+    arm4 = 90;
+    arm5 = 60;
+}
+
+// INIT 
+void init(void){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+}
+
+// DISPLAY
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+
+    glScalef(0.45, 0.45, 0.45);
+
+    // ROTASI 
+    glRotatef((GLfloat)rotateX, 1.0, 0.0, 0.0);
+    glRotatef((GLfloat)rotateY, 0.0, 1.0, 0.0);
+
+    // Shoulder
+    glRotatef((GLfloat)shoulder1, 0.0, 1.0, 0.0);
+    glRotatef((GLfloat)shoulder2, 1.0, 0.0, 0.0);
+    glRotatef((GLfloat)shoulder3, 0.0, 0.0, 1.0);
+
+    // Lengan atas
+    glTranslatef(1.0, 0.0, 0.0);
+    glPushMatrix();
+    glScalef(2.0, 0.4, 1.0);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // Siku
+    glTranslatef(1.0, 0.0, 0.0);
+    glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
+
+    // Lengan bawah
+    glTranslatef(1.0, 0.0, 0.0);
+    glPushMatrix();
+    glScalef(2.0, 0.4, 1.0);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // Pergelangan tangan
+    glTranslatef(1.0, 0.0, 0.0);
+    glRotatef((GLfloat)hand, 0.0, 0.0, 1.0);
+
+    // Telapak tangan
+    glTranslatef(0.5, 0.0, 0.0);
+    glPushMatrix();
+    glScalef(1.0, 0.4, 1.0);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // JARI 
+
+    // Kelingking
+    glPushMatrix();
+    glTranslatef(0.5, 0.0, -0.4);
+    glRotatef((GLfloat)arm1, 0.0, 0.0, 1.0);
+    glTranslatef(0.35, 0.0, 0.0);
+    glScalef(0.7, 0.4, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // Jari manis
+    glPushMatrix();
+    glTranslatef(0.5, 0.0, -0.125);
+    glRotatef((GLfloat)arm2, 0.0, 0.0, 1.0);
+    glTranslatef(0.4, 0.0, 0.0);
+    glScalef(0.8, 0.4, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // Jari tengah
+    glPushMatrix();
+    glTranslatef(0.5, 0.0, 0.125);
+    glRotatef((GLfloat)arm3, 0.0, 0.0, 1.0);
+    glTranslatef(0.5, 0.0, 0.0);
+    glScalef(1.0, 0.4, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // Telunjuk
+    glPushMatrix();
+    glTranslatef(0.5, 0.0, 0.4);
+    glRotatef((GLfloat)arm4, 0.0, 0.0, 1.0);
+    glTranslatef(0.4, 0.0, 0.0);
+    glScalef(0.8, 0.4, 0.2);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    // Ibu jari
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0.5);
+    glRotatef((GLfloat)arm5, 1.0, 0.0, 0.0);
+    glTranslatef(0.0, 0.0, 0.25);
+    glScalef(0.4, 0.2, 0.5);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+// RESHAPE 
+	void reshape(int w, int h){
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(65.0, (GLfloat)w/(GLfloat)h, 1.0, 20.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0, 0.0, -5.0);
+}
+
+// KEYBOARD 
+void keyboard(unsigned char key, int x, int y){
+    switch(key){
+
+        // POSE
+        case '1': poseFist(); break;
+        case '2': poseThumbsUp(); break;
+        case '3': posePeace(); break;
+        case '4': poseOpenHand(); break;
+
+        // ROTASI
+        case 'j': rotateY -= 5; break;
+        case 'l': rotateY += 5; break;
+        case 'i': rotateX -= 5; break;
+        case 'k': rotateX += 5; break;
+
+        // EXIT
+        case 27: exit(0); break;
+    }
+    glutPostRedisplay();
+}
+
+// MAIN 
+int main(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(700, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Emoticon Alebbyoooo");
+
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+}
---
+++ Praktikum 3/Tugas_24060124140176_Aprillia Abel Cleodora/Tugas.cpp
@@ -0,0 +1,189 @@
+/* 
+ * File        : Tugas.cpp
+ * Deskripsi   : Program OpenGL untuk menampilkan model tangan sederhana
+ *               yang terdiri dari lengan, telapak tangan, dan jari-jari.
+ *               Setiap bagian dapat digerakkan menggunakan keyboard.
+ * Pembuat     : Aprillia Abel Cleodora
+ * Tanggal     : 19 Maret 2026
+ */
+ 
+#include <windows.h>
+#include <GL/glut.h>
+
+// Variabel untuk menyimpan sudut rotasi tiap bagian tangan
+static int shoulder1 = 0, shoulder2 = 0, shoulder3 = 0, 
+           elbow = 0, hand = 0, 
+           arm1 = 0, arm2 = 0, arm3 = 0, arm4 = 0, arm5 = 0;
+
+void init(void) {
+	glClearColor (0.0, 0.0, 0.0, 0.0); 
+	glShadeModel (GL_FLAT);            
+}
+
+// Fungsi untuk menampilkan objek
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glPushMatrix();               
+
+	// Transformasi awal
+	glTranslatef(0.0, 0.0, 0.0);
+	glScalef(0.45, 0.45, 0.45); 
+
+	// Rotasi bahu 
+	glRotatef((GLfloat)shoulder1, 0.0, 1.0, 0.0);
+	glRotatef((GLfloat)shoulder2, 1.0, 0.0, 0.0);
+	glRotatef((GLfloat)shoulder3, 0.0, 0.0, 1.0);
+
+	// LENGAN ATAS 
+	glTranslatef(1.0, 0.0, 0.0); 
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0);     
+	glutWireCube(1.0);           
+	glPopMatrix();
+	
+	// SIKU 
+	glTranslatef (1.0, 0.0, 0.0);
+	glRotatef((GLfloat) elbow, 0.0, 0.0, 1.0);
+
+	// LENGAN BAWAH
+	glTranslatef(1.0, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(2.0, 0.4, 1.0); 
+	glutWireCube(1.0); 
+	glPopMatrix();
+	
+	// PERGELANGAN TANGAN 
+	glTranslatef(1.0, 0.0, 0.0);
+	glRotatef((GLfloat)hand, 0.0, 0.0, 1.0);
+
+	// TELAPAK TANGAN 
+	glTranslatef(0.5, 0.0, 0.0);
+	glPushMatrix();
+	glScalef(1.0, 0.4, 1.0);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	// JARI 
+
+	// Jari 1 (kelingking)
+	glPushMatrix();
+	glTranslatef(0.5, 0.0, -0.4);
+	glRotatef((GLfloat)arm1, 0.0, 0.0, 1.0);
+	glTranslatef(0.35, 0.0, 0.0);
+	glScalef(0.7, 0.4, 0.2);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	// Jari 2 (manis)
+	glPushMatrix();
+	glTranslatef(0.5, 0.0, -0.125);
+	glRotatef((GLfloat)arm2, 0.0, 0.0, 1.0);
+	glTranslatef(0.4, 0.0, 0.0);
+	glScalef(0.8, 0.4, 0.2);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	// Jari 3 (tengah)
+	glPushMatrix();
+	glTranslatef(0.5, 0.0, 0.125);
+	glRotatef((GLfloat)arm3, 0.0, 0.0, 1.0);
+	glTranslatef(0.5, 0.0, 0.0);
+	glScalef(1.0, 0.4, 0.2);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	// Jari 4 (telunjuk)
+	glPushMatrix();
+	glTranslatef(0.5, 0.0, 0.4);
+	glRotatef((GLfloat)arm4, 0.0, 0.0, 1.0);
+	glTranslatef(0.4, 0.0, 0.0);
+	glScalef(0.8, 0.4, 0.2);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	// Ibu jari
+	glPushMatrix();
+	glTranslatef(0.0, 0.0, 0.5);
+	glRotatef((GLfloat)arm5, 1.0, 0.0, 0.0);
+	glTranslatef(0.0, 0.0, 0.25);
+	glScalef(0.4, 0.2, 0.5);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();       
+	glutSwapBuffers();  
+}
+
+// Mengatur tampilan viewport dan perspektif
+void reshape(int w, int h) {
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
+	glMatrixMode (GL_PROJECTION); 
+	glLoadIdentity ();
+	gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0); 
+
+	glMatrixMode(GL_MODELVIEW); 
+	glLoadIdentity();
+	glTranslatef (0.0, 0.0, -5.0); 
+}
+
+// Fungsi untuk input keyboard
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+
+		// Rotasi bahu
+		case 'a': shoulder1 = (shoulder1 + 5) % 360; break;
+        case 'A': shoulder1 = (shoulder1 - 5) % 360; break;
+
+		case 's': shoulder2 = (shoulder2 + 5) % 360; break;
+		case 'S': shoulder2 = (shoulder2 - 5) % 360; break;
+
+		case 'd': shoulder3 = (shoulder3 + 5) % 360; break;
+		case 'D': shoulder3 = (shoulder3 - 5) % 360; break;
+
+		// Rotasi siku
+		case 'z': elbow = (elbow + 5) % 360; break;
+        case 'Z': elbow = (elbow - 5) % 360; break;
+
+		// Rotasi pergelangan tangan
+		case 'x': hand = (hand + 5) % 360; break;
+		case 'X': hand = (hand - 5) % 360; break;
+
+		// Rotasi jari
+		case 'q': arm1 = (arm1 + 5) % 360; break;
+		case 'Q': arm1 = (arm1 - 5) % 360; break;
+
+		case 'w': arm2 = (arm2 + 5) % 360; break;
+		case 'W': arm2 = (arm2 - 5) % 360; break;
+
+		case 'e': arm3 = (arm3 + 5) % 360; break;
+		case 'E': arm3 = (arm3 - 5) % 360; break;
+
+		case 'r': arm4 = (arm4 + 5) % 360; break;
+		case 'R': arm4 = (arm4 - 5) % 360; break;
+
+		case 't': arm5 = (arm5 - 5) % 360; break;
+		case 'T': arm5 = (arm5 + 5) % 360; break;
+
+		// Keluar program 
+		case 27: exit(0);
+        default: break;
+    }
+
+	glutPostRedisplay(); 
+}
+
+// MAIN
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB); 
+	glutInitWindowSize (700, 600); 
+	glutInitWindowPosition (100, 100); 
+	glutCreateWindow (argv[0]); 
+
+	init();
+	glutDisplayFunc(display); 
+	glutReshapeFunc(reshape); 
+	glutKeyboardFunc(keyboard); 
+
+	glutMainLoop(); 
+}
