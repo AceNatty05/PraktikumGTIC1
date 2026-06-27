54eb821fea8cd38e4848ff408ab1d3fcea8191aa
AceNatty05 <natasyacandrasari@gmail.com>
2026-06-27 23:47:54 +0700

    asprak: acc Annis

---
+++ Keaktifan/24060124130110_Annis/Tugas1/TesAsprak.cpp
@@ -0,0 +1,283 @@
+#include <GL/glut.h> 
+void P1(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.376f, 0.376f, 0.376f);
+    glRectf(-4.00, 4.00, 4.00, -4.00);
+    glFlush();
+}
+
+void P2(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-4.00, -2.00, -2.00, -4.00);
+    glFlush();
+}
+
+void P3(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-2.00, -2.00, 0.00, -4.00);
+    glFlush();
+}
+
+void P4(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(0.00, -2.00, 2.00, -4.00);
+    glFlush();
+}
+
+void P5(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(2.00, -2.00, 4.00, -4.00);
+    glFlush();
+}
+
+void P6(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(4.00, -2.00, 5.00, -3.00);
+    glFlush();
+}
+
+void P7(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-5.00, -2.00, -4.00, -3.00);
+    glFlush();
+}
+
+void P8(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 0.40f, 0.698f);
+    glRectf(-1.00, -2.00, 0.00, -3.00);
+    glFlush();
+}
+
+void P9(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 0.40f, 0.698f);
+    glRectf(0.00, -2.00, 1.00, -3.00);
+    glFlush();
+}
+
+void P10(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.40f, 0.80f, 0.00f);
+    glRectf(-4.00, -1.00, -3.00, -2.00);
+    glFlush();
+}
+
+void P11(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.00f, 0.00f, 0.00f);
+    glRectf(-3.00, -1.00, -2.00, -2.00);
+    glFlush();
+}
+
+void P12(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.00f, 0.00f, 0.00f);
+    glRectf(-2.00, -1.00, -1.00, -2.00);
+    glFlush();
+}
+
+void P13(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.00f, 0.00f, 0.00f);
+    glRectf(1.00, -1.00, 2.00, -2.00);
+    glFlush();
+}
+
+void P14(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.00f, 0.00f, 0.00f);
+    glRectf(2.00, -1.00, 3.00, -2.00);
+    glFlush();
+}
+
+void P15(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.40f, 0.80f, 0.00f);
+    glRectf(3.00, -1.00, 4.00, -2.00);
+    glFlush();
+}
+
+void P16(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-1.00, -1.00, 0.00, -2.00);
+    glFlush();
+}
+
+void P17(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(0.00, -1.00, 1.00, -2.00);
+    glFlush();
+}
+
+void P18(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-1.00, 0.00, 0.00, -1.00);
+    glFlush();
+}
+
+void P19(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(0.00, 0.00, 1.00, -1.00);
+    glFlush();
+}
+
+void P20(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-1.00, 1.00, 0.00, 0.00);
+    glFlush();
+}
+
+void P21(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(0.00, 1.00, 1.00, 0.00);
+    glFlush();
+}
+
+void P22(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.00f, 1.00f, 1.00f);
+    glRectf(-0.50, 2.00, 0.50, 1.00);
+    glFlush();
+}
+
+void P23(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(-2.00, 0.00, -1.00, -1.00);
+    glFlush();
+}
+
+void P24(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(-2.00, 1.00, -1.00, 0.00);
+    glFlush();
+}
+
+void P25(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(1.00, 1.00, 2.00, 0.00);
+    glFlush();
+}
+
+void P26(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(1.00, 0.00, 2.00, -1.00);
+    glFlush();
+}
+
+void P27(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.376f, 0.376f, 0.376f);
+    glRectf(4.00, 0.00, 5.00, -1.00);
+    glFlush();
+}
+
+void P28(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.376f, 0.376f, 0.376f);
+    glRectf(-5.00, 0.00, -4.00, -1.00);
+    glFlush();
+}
+
+void P29(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(0.50, 2.00, 1.50, 1.00);
+    glFlush();
+}
+
+void P30(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(-1.50, 2.00, -0.50, 1.00);
+    glFlush();
+}
+
+void P31(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(-0.50, 4.00, 0.50, 3.00);
+    glFlush();
+}
+
+void P32(void) {
+//    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.627f, 0.627f, 0.627f);
+    glRectf(-0.50, 3.00, 0.50, 2.00);
+    glFlush();
+}
+
+//void P33(void) {
+////    glClear(GL_COLOR_BUFFER_BIT);
+//    glColor3f(0.502f, 0.502f, 0.502f);
+//    glRectf(-0.50, 3.00, 0.50, 2.00);
+//    glFlush();
+//}
+
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	P1();
+	P2();
+	P3();
+	P4();
+	P5();
+	P6();
+	P7();
+	P8();
+	P9();
+	P10();
+	P11();
+	P12();
+	P13();
+	P14();
+	P15();
+	P16();
+	P17();
+	P18();
+	P19();
+	P20();
+	P21();
+	P22();
+	P23();
+	P24();
+	P25();
+	P26();
+	P27();
+	P28();
+	P29();
+	P30();
+	P31();
+	P32();
+}
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Kucing");
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-7.0, 7.0, -7.0, 7.0);
+    glutDisplayFunc(display);
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glutMainLoop();
+    return 0;
+}
